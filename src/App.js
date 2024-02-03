e9aa3dc43eabc8958289f88eb3cc672e508a05f5
Sefensi <sefensi_reload@mail.ru>
2024-02-03 15:48:45 +0300

    Управляемы и неуправляемый компоненты

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "Javascript", body: "Description" },
     { id: 2, title: "Javascript 2", body: "Description" },
     { id: 3, title: "Javascript 3", body: "Description" },
   ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript 2", body: "Description" },
-    { id: 3, title: "Javascript 3", body: "Description" },
-  ]);
+  const [title, setTitle] = useState("");
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  };
+
   return (
     <div className="App">
-      <PostList posts={posts} title={'First list'}/>
-      <PostList posts={posts2} title={'Second list'}/>
+      <form>
+        {/* Управляемый компонет */}
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Post name"
+        />
+        {/* Неуправляемый компонент */}
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Post information"
+        />
+        <MyButton onClick={addNewPost}>Create a post</MyButton>
+      </form>
+      <PostList
+        posts={posts}
+        title={"First list"}
+      />
     </div>
   );
 }
