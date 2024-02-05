ad66c4968353cbfeeb47f98ecc346511aa73493a
Sefensi <sefensi_reload@mail.ru>
2024-02-05 23:14:49 +0300

    Исправление ошибок и добавление всплывающего окна

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useMemo, useState } from "react";
 import "./styles/App.css";
-
 import PostList from "./components/PostList";
-
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 function App() {
   const [posts, setPosts] = useState([
     { id: Date.now(), title: "aaa", body: "lsdadaf" },
@@ -14,6 +12,7 @@ function App() {
     { id: Date.now() + 2, title: "jjjjj 3", body: "qqasdqe" },
   ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     console.log("Check sorting function");
@@ -34,6 +33,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false)
   };
   // Получаем  post  из дочернего элемента с
   // помощью callback,
@@ -44,19 +44,23 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{marginTop: 30}} onClick={()=> setModal(true)}>
+      Create a user
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
-      
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title={"Posts"}
-        />
-       
+
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title={"Posts"}
+      />
     </div>
   );
 }
