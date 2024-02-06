9d5c72c5e01b99a08132f6e5b3d3c185f79b06c9
Sefensi <sefensi_reload@mail.ru>
2024-02-06 19:36:38 +0300

    Реализация try/catch

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./APi/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 function App() {
-  const [posts, setPosts] = useState([
-    { id: Date.now(), title: "aaa", body: "lsdadaf" },
-    { id: Date.now() + 1, title: "ccccc 2", body: "zxcq" },
-    { id: Date.now() + 2, title: "jjjjj 3", body: "qqasdqe" },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-  const sortedPosts = useMemo(() => {
-    console.log("Check sorting function");
-
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort]),
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase()),
-    );
-  }, [filter.query, sortedPosts]);
+  const [fetchPost, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(false)
+    setModal(false);
   };
-  // Получаем  post  из дочернего элемента с
-  // помощью callback,
-  // также как и в create(выше)
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={()=> setModal(true)}>
-      Create a user
+      {/* <button onClick={fetchPost}>GET POSTS</button> */}
+      <MyButton
+        style={{ marginTop: 30 }}
+        onClick={() => setModal(true)}
+      >
+        Create a user
       </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
+      <MyModal
+        visible={modal}
+        setVisible={setModal}
+      >
         <PostForm create={createPost} />
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
@@ -55,12 +53,20 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={"Posts"}
-      />
+      {postError && <h1>Error: ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 200 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title={"Posts"}
+        />
+      )}
     </div>
   );
 }
